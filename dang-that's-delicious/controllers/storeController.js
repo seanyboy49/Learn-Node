90cefa042df6a944cc271af6d05abb6cd651117f
Sean Lee <seanboramlee@gmail.com>
2017-12-12 23:06:44 -0800

    add editStore and createStore handlers in storeController as well as as _storeForm mixin

--- dang-that's-delicious/controllers/storeController.js
+++ dang-that's-delicious/controllers/storeController.js
@@ -1,5 +1,13 @@
 
 exports.homePage = (req,res) => {
     console.log(req.name)
-    res.render('index');
+    res.render('index', {title: "Home Page"});
+}
+
+exports.addStore = (req, res) => {
+    res.render('editStore', {title: 'Add Store'})
+}
+
+exports.createStore = (req, res) => {
+    res.json(req.body); // contains all the information sent in the form
 }
